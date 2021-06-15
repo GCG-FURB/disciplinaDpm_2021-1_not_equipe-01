edd171f1a7ee846b8992a435d40c91b94cf92dd2
Rennã-Tiedt-FAEL <renna.tiedt@fael.edu.br>
2021-06-14 21:32:15 -0300

    estou seguindo a Jesus Cristo

---
+++ unidade_4/projeto_final/lib/src/bloc/LembretesBloc.dart
@@ -0,0 +1,11 @@
+import 'package:bloc_pattern/bloc_pattern.dart';
+
+// class PersonBloc extends BlocBase {
+//   var _repository = AppModule.to.getDependency<PersonRepository>();
+//   get people => _repository.people;
+
+//   @override
+//   void dispose() {
+//     super.dispose();
+//   }
+// }
